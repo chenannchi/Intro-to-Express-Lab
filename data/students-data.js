9c9d5e4b85706cc150dd1b74770fbf95abdf10cf
chenannchi <anchichenusa@gmail.com>
2022-09-19 21:34:42 -0400

    finish the lab

---
+++ data/students-data.js
@@ -0,0 +1,15 @@
+const students = [
+  {name:"Youngbin", yearOfBirth:1993, id:1001},
+  {name:"Inseong", yearOfBirth:1993, id:1002},
+  {name:"Taeyang", yearOfBirth:1997, id: 1003},
+  {name:"Jaeyoon", yearOfBirth:1994, id: 1004},
+  {name:"Dawon", yearOfBirth:1995, id: 1005},
+  {name:"Rowoon", yearOfBirth:1996, id: 1006},
+  {name:"Zuho", yearOfBirth:1996, id: 1007},
+  {name:"Hwiyoung", yearOfBirth:1999, id: 1008},
+  {name:"Chani", yearOfBirth:2000, id: 1009},
+]
+
+export{
+  students
+}
